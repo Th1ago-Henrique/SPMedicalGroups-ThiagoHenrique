c8c95be714808e94eee6b6f3498e11c6a914dbea
FIC <FIC@info.local>
2021-08-23 11:22:52 -0300

    .

---
+++ Scipts/senai_spmedgroup_01_DQL.sql
@@ -0,0 +1,26 @@
+USE ThiagoSPMG
+GO
+
+SELECT * FROM clinica$;
+GO
+
+SELECT * FROM tipoPermissao$;
+GO
+
+SELECT * FROM usuario$;
+GO
+
+SELECT * FROM especialidade$;
+GO
+
+SELECT * FROM medico$;
+GO
+
+SELECT * FROM paciente$;
+GO
+
+SELECT * FROM situacao$;
+GO
+
+SELECT * FROM consulta$;
+GO
